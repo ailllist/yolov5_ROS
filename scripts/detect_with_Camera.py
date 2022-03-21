bc0ebfdfa5f54fa1f31e70bd9fb289e14a6fb5d2
Lab-Desktop-Window <hojun060534@gmail.com>
2022-03-21 16:52:14 +0900

    testing in windows
    for testing

--- scripts/detect_with_Camera.py
+++ scripts/detect_with_Camera.py
@@ -49,14 +49,16 @@ from utils.general import (LOGGER, check_file, check_img_size, check_requirement
 from utils.plots import Annotator, colors, save_one_box
 from utils.torch_utils import select_device, time_sync
 
-IMGSZ = (640, 480)
+# IMGSZ = (640, 480)
+IMGSZ = (1920, 1080)
 FPS = 90 # 0 -> as much as possable (default)
 
 @torch.no_grad()
-def run(weights=ROOT / 'yolov5s.pt',  # model.pt path(s)
-        data=ROOT / 'data/coco128.yaml',  # dataset.yaml path
-        conf_thres=0.6,  # confidence threshold
-        iou_thres=0.45,  # NMS IOU threshold
+# def run(weights=ROOT / 'yolov5s.pt',  # model.pt path(s)
+def run(weights=ROOT / 'best_0321.pt',  # model.pt path(s)
+        # data=ROOT / 'data/coco128.yaml',  # dataset.yaml path
+        conf_thres=0,  # confidence threshold
+        iou_thres=0,  # NMS IOU threshold
         max_det=1000,  # maximum detections per image
         device='',  # cuda device, i.e. 0 or 0,1,2,3 or cpu
         line_thickness=3,  # bounding box thickness (pixels)
@@ -66,7 +68,8 @@ def run(weights=ROOT / 'yolov5s.pt',  # model.pt path(s)
 
     # Load model
     device = select_device(device)
-    model = DetectMultiBackend(weights, device=device, dnn=False, data=data)
+    # model = DetectMultiBackend(weights, device=device, dnn=False, data=data)
+    model = DetectMultiBackend(weights, device=device, dnn=False)
     stride, names, pt, jit, onnx, engine = model.stride, model.names, model.pt, model.jit, model.onnx, model.engine
     imgsz = check_img_size(IMGSZ, s=stride)  # check image size
 
@@ -84,6 +87,7 @@ def run(weights=ROOT / 'yolov5s.pt',  # model.pt path(s)
     # Run inference
     model.warmup(imgsz=(1 if pt else bs, 3, *imgsz), half=half)  # warmup
     dt, seen = [0.0, 0.0, 0.0], 0
+
     while True:
         s_time = time.time()
         im, im0, vid_cap, s = dataset.return_info()
@@ -140,7 +144,7 @@ def run(weights=ROOT / 'yolov5s.pt',  # model.pt path(s)
             im0 = annotator.result()
         cv2.imshow("res", im0)
         # cv2.imwrite("res1.png", im0)
-        cv2.waitKey(1)  # 1 millisecond
+        cv2.waitKey(1000)  # 1 millisecond
         # Print time (inference-only)
         # LOGGER.info(f'{s}Done. ({t3 - t2:.3f}s)')
         # print(1/(time.time()-s_time), time.time()-s_time)
--- scripts/utils/useing_video.py
+++ scripts/utils/useing_video.py
@@ -1,6 +1,10 @@
 import pyrealsense2 as rs
 import numpy as np
 import cv2
+import os
+
+target = "folder"
+TARGET_FOLDER = r"C:\Users\1234\Desktop\Wego_2021\DATASET\img"
 
 # Configure depth and color streams
 class Camera:
@@ -9,8 +13,11 @@ class Camera:
         """
         Camera initializing part, usually insert here your camera code except loop part(l.e. get next frame)
         """
-        self.vid = cv2.VideoCapture("")
+        if target == "Video":
+            self.vid = cv2.VideoCapture("")
 
+        elif target == "folder":
+            self.img_list = os.listdir(TARGET_FOLDER)
 
     def get_next_img(self) -> np.ndarray:
 
@@ -21,6 +28,12 @@ class Camera:
         """
 
         # Wait for a coherent pair of frames: depth and color
-        _, frame = self.vid.read()
+        if target == "Video":
+            _, frame = self.vid.read()
 
-        return frame
+            return frame
+
+        elif target == "folder":
+            img = cv2.imread(fr"{TARGET_FOLDER}\{self.img_list[0]}")
+            del self.img_list[0]
+            return img
