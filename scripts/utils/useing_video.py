a386a815673ce0cbf9851c7e92f113581cf6fa05
Lab-Labtop-Linux <hojun060534@gmail.com>
2022-05-13 19:38:14 +0900

    add using video case (banchmark)

--- scripts/utils/useing_video.py
+++ scripts/utils/useing_video.py
@@ -1,10 +1,11 @@
-import pyrealsense2 as rs
+# import pyrealsense2 as rs
 import numpy as np
 import cv2
 import os
 
-target = "folder"
-TARGET_FOLDER = r"C:\Users\1234\Desktop\Wego_2021\DATASET\img"
+target = "Video"
+TARGET_VIDEO = r"/home/autonav-linux/catkin_ws/src/yolov5_ROS/scripts/traffic_light_test.mp4"
+TARGET_FOLDER = r"/home/autonav-linux/catkin_ws/src/yolov5_ROS/scripts/images"
 
 # Configure depth and color streams
 class Camera:
@@ -14,7 +15,7 @@ class Camera:
         Camera initializing part, usually insert here your camera code except loop part(l.e. get next frame)
         """
         if target == "Video":
-            self.vid = cv2.VideoCapture("")
+            self.vid = cv2.VideoCapture(TARGET_VIDEO)
 
         elif target == "folder":
             self.img_list = os.listdir(TARGET_FOLDER)
@@ -30,10 +31,9 @@ class Camera:
         # Wait for a coherent pair of frames: depth and color
         if target == "Video":
             _, frame = self.vid.read()
-
             return frame
 
         elif target == "folder":
-            img = cv2.imread(fr"{TARGET_FOLDER}\{self.img_list[0]}")
+            img = cv2.imread(fr"{TARGET_FOLDER}/{self.img_list[0]}")
             del self.img_list[0]
             return img
